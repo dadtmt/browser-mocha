fe3d139536f1e4e3586bf3c27135e0c2c714d9d9
tmt <thomas.culdaut@gmail.com>
2018-04-15 18:06:00 +0200

    makeGamersList refactor fails

--- public/gamers.js
+++ public/gamers.js
@@ -1,11 +1,11 @@
-export const makeGamer = gamer => `
-<li>${gamer.firstName} plays ${gamer.game}</li>`
+export const makeGamer = gamer => `<li>${gamer.firstName} plays ${gamer.game}</li>`
 
-const makeGamersList = gamers => `
-  <ul>
-    <li>Thomas plays XCOM</li>
-    <li>Clément plays GTA</li>
-   </ul>
-`
+const makeGamersList = gamers => {
+  const lis = ''
+  
+  return `<ul>
+      ${lis}
+    </ul>`
+  }
 
 export default makeGamersList
