8138049785812c4ac74afda4c8cd539c34225f09
tmt <thomas.culdaut@gmail.com>
2018-04-15 15:42:40 +0200

    fail second test in browser

--- public/gamers.test.js
+++ public/gamers.test.js
@@ -1,19 +1,28 @@
 import makeGamsersList from './gamers.js'
 
 const someGamers = [
-    {
-        firstName: 'Thomas',
-        game: 'XCOM'
-    },
-    {
-        firstName: 'Clément',
-        game: 'GTA'
-    }
+  {
+      firstName: 'Thomas',
+      game: 'XCOM'
+  },
+  {
+      firstName: 'Clément',
+      game: 'GTA'
+  }
 ]
 
+const expectedHtml = `
+  <ul>
+    <li>Thomas plays XCOM</li>
+    <li>Clément plays GTA</li>
+  </ul>
+`
 
 describe('makeGamersList', () => {
-    it('should return a string', () => {
-        chai.assert.typeOf(makeGamsersList(someGamers), 'string')
-    })
+  it('should return a string', () => {
+      chai.assert.typeOf(makeGamsersList(someGamers), 'string')
+  })
+  it ('should return an html string of a gamers list', () => {
+      chai.assert.equal(makeGamsersList(someGamers), expectedHtml)
+  })
 })
